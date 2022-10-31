aec1dc2b9575cddad27ef0c04edcb4c6f31b202d
kkelleaz17 <111470082+kkelleaz17@users.noreply.github.com>
2022-10-31 16:24:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,21 @@
 var RN = (MAX)=>{
     return Math.floor(Math.random()*MAX)+1;
 }
-
+///// CREATES RANDOM
 
 
 const PUZZLELENGTH = 11;
 var PUZZLENUMBER = 0;
 var SCORE = 0;
-
 var PUZZLELIST = [];
-
+var PICTUREID1 = "";
+var PICTUREID2 = "";
 
 
 /// on load 
 
 RandomPictures(RN(PUZZLELENGTH));
-SHOWORNOT(1,33,"none");
+REMOVEOB(1,9,"none");
 
 ///
 
@@ -26,17 +26,16 @@ SHOWORNOT(1,33,"none");
 
 
 function RandomPictures(PN){
-    console.log(PUZZLELIST.includes(PN) == false && PUZZLELIST.length+1 != PUZZLELENGTH)
-    if(PUZZLELIST.includes(PN) == false && PUZZLELIST.length+1 != PUZZLELENGTH){
+    if(PUZZLELIST.includes(PN) == false && PUZZLELIST.length != PUZZLELENGTH){
         PUZZLELIST.push(PN);
-    PUZZLENUMBER = PN
+    PUZZLENUMBER = PN;
     document.getElementById("PUZZ").innerHTML = "Puzzle: "+PUZZLENUMBER;
     var OPEN = "";
     for(let i =1;i<=9;i++){
         do {
             var RND = RN(9);
         }while(OPEN.includes(RND) == true);
-        var FILELOCATION = "/Puzzle"+PN+"/image_part_00"+RND+".jpg"
+        var FILELOCATION = "./Puzzle"+PN+"/image_part_00"+RND+".jpg"
         document.getElementById("IMG"+i).src = FILELOCATION;
         document.getElementById("IMG"+i).classList.add("MOVEMENT"+RN(4));
         OPEN=OPEN+RND;
@@ -44,15 +43,10 @@ function RandomPictures(PN){
 }else{
     if(PUZZLELIST.length+1 == PUZZLELENGTH){
         PUZZLELIST = [];
-        console.log(PUZZLELIST)
-    }else{
-    RandomPictures(RN(PUZZLELENGTH));
+        RandomPictures(RN(PUZZLELENGTH));
     }
 }
 }
-
-
-
 function ROTATE(PICTURE){
 var CP = document.getElementById("IMG"+PICTURE);
 if(CP.classList.contains("MOVEMENT1") == true){
@@ -70,43 +64,17 @@ else if(CP.classList.contains("MOVEMENT4") == true){
     CP.classList.remove("MOVEMENT4");
     CP.classList.add("MOVEMENT1");
 }
-SHOWORNOT(1,33,"none");
+REMOVEOB(1,9,"none");
 }
-
-
-
-function SWITICH(CURRENT,SWITCHTOO){
-var A = document.getElementById("IMG"+CURRENT);
-var B = document.getElementById("IMG"+SWITCHTOO);
-var ACLASS = A.className;
-var BCLASS = B.className;
-A.className = BCLASS;
-B.className = ACLASS;
-var TEMP = A.src;
-A.src = B.src;
-B.src=TEMP;
-SHOWORNOT(1,33,"none");
-}
-
-
-document.getElementById("IMG1").addEventListener("click",()=>{SHOWORNOT(1,33,"none");SHOWORNOT(1,3,"")});
-document.getElementById("IMG2").addEventListener("click",()=>{SHOWORNOT(1,33,"none");SHOWORNOT(4,7,"")});
-document.getElementById("IMG3").addEventListener("click",()=>{SHOWORNOT(1,33,"none");SHOWORNOT(8,10,"")});
-document.getElementById("IMG4").addEventListener("click",()=>{SHOWORNOT(1,33,"none");SHOWORNOT(11,14,"")});
-document.getElementById("IMG5").addEventListener("click",()=>{SHOWORNOT(1,33,"none");SHOWORNOT(15,19,"")});
-document.getElementById("IMG6").addEventListener("click",()=>{SHOWORNOT(1,33,"none");SHOWORNOT(20,23,"")});
-document.getElementById("IMG7").addEventListener("click",()=>{SHOWORNOT(1,33,"none");SHOWORNOT(24,26,"")});
-document.getElementById("IMG8").addEventListener("click",()=>{SHOWORNOT(1,33,"none");SHOWORNOT(27,30,"")});
-document.getElementById("IMG9").addEventListener("click",()=>{SHOWORNOT(1,33,"none");SHOWORNOT(31,33,"")});
-
-function SHOWORNOT(START,END,VALUE){
-  for(let i = START;i<END+1;i++){
-    document.getElementById("IMG-OB"+i).style.display = VALUE;
-  }
-}
-
-
-
+document.getElementById("IMG1").addEventListener("click",(e)=>{REMOVEOB(1,9,"none");REMOVEOB(1,1,"");});
+document.getElementById("IMG2").addEventListener("click",(e)=>{REMOVEOB(1,9,"none");REMOVEOB(2,2,"");});
+document.getElementById("IMG3").addEventListener("click",(e)=>{REMOVEOB(1,9,"none");REMOVEOB(3,3,"");});
+document.getElementById("IMG4").addEventListener("click",(e)=>{REMOVEOB(1,9,"none");REMOVEOB(4,4,"");});
+document.getElementById("IMG5").addEventListener("click",(e)=>{REMOVEOB(1,9,"none");REMOVEOB(5,5,"");});
+document.getElementById("IMG6").addEventListener("click",(e)=>{REMOVEOB(1,9,"none");REMOVEOB(6,6,"");});
+document.getElementById("IMG7").addEventListener("click",(e)=>{REMOVEOB(1,9,"none");REMOVEOB(7,7,"");});
+document.getElementById("IMG8").addEventListener("click",(e)=>{REMOVEOB(1,9,"none");REMOVEOB(8,8,"");});
+document.getElementById("IMG9").addEventListener("click",(e)=>{REMOVEOB(1,9,"none");REMOVEOB(9,9,"");});
 document.getElementById('COMPLETE').addEventListener("click",()=>{
      var CHECK = 0;
  for(let i = 1;i<10;i++){
@@ -128,15 +96,6 @@ document.getElementById('COMPLETE').addEventListener("click",()=>{
     RandomPictures(RN(PUZZLELENGTH));
     },1000)
  }
-
-
-
-
-
-
-
-
-
 document.getElementById("SCORE").innerHTML = "Score: "+SCORE;
 })
 
@@ -148,3 +107,37 @@ document.getElementById("RESET").addEventListener("click",()=>{
 document.getElementById("NEW").addEventListener("click",()=>{
     RandomPictures(RN(PUZZLELENGTH));
 })
+function REMOVEOB(MIN,MAX,ITEM){
+for(let i = MIN;i<MAX+1;i++){
+document.getElementById("IMG-OB"+i).style.display = ITEM;
+}
+}
+function ONDRAG(event){
+    PICTUREID1 = event.target.id;
+}
+function DROP(e){ 
+PICTUREID2 = e.target.id;
+SWITCHPICTURES()
+}
+function allowDrop(event) {
+  event.preventDefault();
+}
+function SWITCHPICTURES(){
+REMOVEOB(1,9,"none");
+var ELEM1 =document.getElementById(PICTUREID1);
+var ELEM2 =document.getElementById(PICTUREID2);
+var A = ELEM1.src
+var B = ELEM2.src
+var TEMP = A;
+A = B;
+B = TEMP;
+var ACLASS = ELEM1.className;
+var BCLASS = ELEM2.className;
+var TEMPCLASS = ACLASS;
+ACLASS = BCLASS
+BCLASS = TEMPCLASS 
+document.getElementById(PICTUREID1).src = A;
+document.getElementById(PICTUREID2).src = B;
+document.getElementById(PICTUREID2).className = ACLASS;
+document.getElementById(PICTUREID2).className = BCLASS;
+}
